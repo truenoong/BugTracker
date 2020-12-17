e73b2e06cecf817d23e0366a8a61bbbf62952811
truenoong <trueno_ady@hotmail.com>
2020-12-17 20:37:14 +0800

    firebase failed... oh well time to switch to sql

--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -2,4 +2,16 @@
 
 @section('content')
     <h1>{{$title ?? 'Projects'}}</h1>
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+          </tr>
+        </thead>
+        <tbody id="tbody">
+        </tbody>
+      </table>
 @endsection
