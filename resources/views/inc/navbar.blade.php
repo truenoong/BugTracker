c52206013488f4d7396e7e9346f4e4c3a61287b3
truenoong <trueno_ady@hotmail.com>
2020-12-23 02:34:52 +0800

    logout feature completed

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,4 +1,3 @@
-
 @guest
 
 <nav class="navbar horizontal-navbar navbar-expand-lg navbar-light">
@@ -7,10 +6,10 @@
         </ul>
         <ul class="rightNavbar navbar-nav ml-auto">
             <li id="dashboard" class="nav-item">
-                <a href="/" id="login" class="icon-search icon-nav nav-link text-dark font-italic nav-text">Login</a>
+                <a href="/" id="login" class="icon-search icon-nav nav-link text-dark nav-text horizontal-nav-text">Login</a>
             </li>   
             <li id="dashboard" class="nav-item">
-                <a href="/register" id="register" class="icon-profile icon-nav nav-link text-dark font-italic nav-text">Register</a>
+                <a href="/register" id="register" class="icon-profile icon-nav nav-link text-dark nav-text horizontal-nav-text">Register</a>
             </li>
         </ul>
     </div>
@@ -28,12 +27,12 @@
 
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <a href="/profile" id="profile" class="icon-search icon-nav nav-link text-dark font-italic">
+                <a href="/profile" id="profile" class="icon-search icon-nav nav-link text-dark">
                     <svg fill="#f0f0f0" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px"><path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"/></svg>
                 </a>
             </form>
 
-            <a id="profile" class="icon-profile icon-nav nav-link text-dark font-italic">
+            <a id="profile" class="icon-profile icon-nav nav-link text-dark">
                 <img class="nav-icon profile-icon" src="{{ asset('img/profile.png') }}">
             </a>
             <a href="{{ route('logout') }}" class="horizontal-nav-text logout-margin"
@@ -53,25 +52,25 @@
 <div class="vertical-nav" id="sidebar">
     <ul class="nav flex-column mb-0">
         <li id="dashboard" class="nav-item">
-            <a href="/dashboard" class="icon-nav nav-link text-dark font-italic">
+            <a href="/dashboard" class="icon-nav nav-link text-dark">
                 <img class="nav-icon" src="{{ asset('img/dashboard.png') }}">
                 <p class="nav-text">Dashboard</p>
             </a>
         </li>
         <li class="nav-item">
-            <a href="/users" id="user" class="icon-nav nav-link text-dark font-italic">
+            <a href="/users" id="user" class="icon-nav nav-link text-dark">
                 <img class="nav-icon" src="{{ asset('img/user.png') }}">
                 <p class="nav-text">Users</p>
             </a>
         </li>
         <li class="nav-item">
-            <a href="/projects" id="project" class="icon-nav nav-link text-dark font-italic">
+            <a href="/projects" id="project" class="icon-nav nav-link text-dark">
                 <img class="nav-icon" src="{{ asset('img/project.png') }}">
                 <p class="nav-text">Projects</p>
             </a>
         </li>
         <li class="nav-item">
-            <a href="/tickets" id="ticket" class="icon-nav nav-link text-dark font-italic">
+            <a href="/tickets" id="ticket" class="icon-nav nav-link text-dark">
                 <img class="nav-icon" src="{{ asset('img/bug.png') }}">
                 <p class="nav-text">Tickets</p>
             </a>
